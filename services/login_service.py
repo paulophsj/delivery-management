7c48d255eac7461be2187078d8421929aeeff8de
paulophsj <snoopyphs@gmail.com>
2026-05-13 12:05:46 -0300

    feat: refatorando a view de login e criando o service para iniciar o tratamento das solicitacoes

--- services/login_service.py
+++ services/login_service.py
@@ -1,4 +1,20 @@
+from views.login_view import LoginView
+
+def auth(email, senha):
+    print(email, senha)
+
 class LoginService:
     def __init__(self, events, values):
         self.events = events
         self.values = values
+
+        routes = {
+            LoginView.login_btn_signin: lambda: auth(
+                self.values[LoginView.login_input_email],
+                self.values[LoginView.login_input_senha])
+        }
+
+        router_func = routes.get(events)
+
+        if router_func:
+            router_func()
--- views/login_view.py
+++ views/login_view.py
@@ -5,12 +5,66 @@ sg.theme_add_new("White", white_theme)
 sg.theme("White")
 
 class LoginView:
-    login_key_view = "-LOGIN_VIEW-"
+    login_key_view = "-LOGIN_KEY_VIEW-"
+    login_input_email = "-LOGIN_INPUT_EMAIL-"
+    login_input_senha = "-LOGIN_INPUT_SENHA-"
+    login_btn_signin = "-LOGIN_BTN_SIGNIN-"
 
     def __init__(self):
         pass
 
     def render(self):
-        return sg.Column([
-            [sg.Text("LOGIN PAGE")]
-        ], key=self.login_key_view, expand_y=True, visible=False)
+
+        header = [
+            [sg.Text("Bem-vindo",
+                     pad=(0, (0, 2)))],
+            [sg.Text("Acesse sua conta para continuar",
+                     pad=(0, (0, 16)))],
+        ]
+
+        form = [
+            [sg.Text("E-mail", pad=(0, (0, 4)))],
+            [sg.Input(
+                key=self.login_input_email,
+                size=(32, 1),
+                enable_events=True,
+                expand_x=True,
+            )],
+            [sg.Text("Senha", pad=(0, (0, 4)))],
+            [sg.Input(
+                password_char="•",
+                key=self.login_input_senha,
+                enable_events=True,
+                size=(32, 1),
+                expand_x=True
+            )],
+        ]
+
+        btn_row = [
+            [sg.Button(
+                "  Entrar  ",
+                key=self.login_btn_signin,
+                size=(28, 1),
+                font="Arial 15",
+                button_color=("White", "Brown"),
+                border_width=1,
+                pad=(0, (40, 12)),
+            )]
+        ]
+
+        card_layout = header + form + btn_row
+
+        return sg.Column(
+            [[sg.Frame(
+                "",
+                card_layout,
+                pad=(24, 24),
+                relief=sg.RELIEF_FLAT,
+                border_width=1,
+            )]],
+            key=self.login_key_view,
+            element_justification="center",
+            expand_y=True,
+            expand_x=True,
+            visible=False,
+        )

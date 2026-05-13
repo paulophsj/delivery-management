f105429da1d64725e2300957b0d18525c277eb01
paulophsj <paulosilvajunior2004@gmail.com>
2026-05-13 19:41:08 -0300

    feat: iniciando regra de negocio para login de usuario

--- services/login_service.py
+++ services/login_service.py
@@ -1,7 +1,30 @@
 from views.login_view import LoginView
+from utils.file_util import load_data, users_path
+from utils.user_type_util import UserType
+
+from app_controller import app
 
 def auth(email, senha):
-    print(email, senha)
+    users = load_data(users_path)
+
+    find_user = next(
+        (user for user in users if user["email"] == email),
+        None
+    )
+
+    if find_user == None:
+        return # tratativa de erro posterior
+    
+    if find_user["senha"] != senha:
+        app.throw_error("Senha incorreta!")
+        return
+    
+    if find_user["role"] == UserType.CLIENTE.value:
+        app.towards("") # rota para cliente
+    elif find_user["role"] == UserType.ADMIN.value:
+        app.towards("") # rota para administrador
+    else:
+        app.towards("") # rota de role nao encontrada
 
 class LoginService:
     def __init__(self, events, values):

6c4b04c1410ed46e3517cbb1b2836d36a7bc0174
paulophsj <snoopyphs@gmail.com>
2026-05-18 16:06:45 -0300

    feat: criando roteamento das paginas com seta de paginas

---
+++ services/router_service.py
@@ -0,0 +1,37 @@
+from views.router_view import RouterView
+
+from app_controller import app
+
+def forwards():
+    if app.routing_index < len(app.routing) - 1:
+        app.routing_index += 1
+        next_page = app.routing[app.routing_index]
+
+        app.window[app.current_page].update(visible=False)
+        app.window[next_page].update(visible=True)
+        app.current_page = next_page
+
+
+def backwards():
+    if app.routing_index > 0:
+        app.routing_index -= 1
+        previous_page = app.routing[app.routing_index]
+
+        app.window[app.current_page].update(visible=False)
+        app.window[previous_page].update(visible=True)
+        app.current_page = previous_page
+
+class RouterService:
+    def __init__(self, events, values):
+        self.events = events
+        self.values = values
+
+        routes = {
+            RouterView.router_btn_forward: lambda : forwards(),
+            RouterView.router_btn_back: lambda : backwards()
+        }
+
+        router_func = routes.get(events)
+
+        if router_func:
+            router_func()

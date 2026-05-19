6a1bd832b1810770820553f1478fd368773f7aaa
paulophsj <snoopyphs@gmail.com>
2026-05-19 15:48:31 -0300

    feat: adicionando busca de produtos pelo nome na parte de admin_cardapio_view.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from services.login_service import LoginService
 from services.register_service import RegisterService
 from services.router_service import RouterService
 from services.dashboard_services.admin_dashboard_service import AdminDashboardService
+from services.dashboard_services.admin_services.admin_cardapio_service import AdminCardapioService
 import PySimpleGUI as sg
 
 while True:
@@ -27,5 +28,8 @@ while True:
 
         case e if e.startswith("-DASHBOARD_ADMIN"):
             AdminDashboardService(events, values)
+
+        case e if e.startswith("-ADMIN_CARDAPIO"):
+            AdminCardapioService(events, values)
 
 app.window.close()

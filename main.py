5be8552ed3f011371e58cd1a5408d3bd0d0521ca
paulo.henrique <paulo.henrique@pneubras.com>
2026-06-05 08:25:53 -0300

    feat: fluxo de pedido do cliente e bootstrap do Tcl/Tk
    - view de cardapio do cliente com carrinho (qtd/obs) e total
    - service monta o pedido, registra a venda e incrementa num_pedidos
    - dashboard do cliente roteia para novo pedido e perfil
    - main.py registra a rota -DASHBOARD_CLIENT e define TCL/TK_LIBRARY

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
+import sys
+
+_tcl_base = os.path.join(sys.base_prefix, "tcl")
+os.environ.setdefault("TCL_LIBRARY", os.path.join(_tcl_base, "tcl8.6"))
+os.environ.setdefault("TK_LIBRARY", os.path.join(_tcl_base, "tk8.6"))
+
 from app_controller import app
 from services.index_service import IndexService
 from services.login_service import LoginService
 from services.register_service import RegisterService
 from services.router_service import RouterService
 from services.dashboard_services.admin_dashboard_service import AdminDashboardService
+from services.dashboard_services.client_dashboard_service import ClientDashboardService
 from services.dashboard_services.admin_services.admin_cardapio_service import AdminCardapioService
 import PySimpleGUI as sg
 
@@ -28,6 +36,9 @@ while True:
 
         case e if e.startswith("-DASHBOARD_ADMIN"):
             AdminDashboardService(events, values)
+
+        case e if e.startswith("-DASHBOARD_CLIENT"):
+            ClientDashboardService(events, values)
 
         case e if e.startswith("-ADMIN_CARDAPIO"):
             AdminCardapioService(events, values)
